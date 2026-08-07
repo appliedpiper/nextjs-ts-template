d022ccb86dbea011fc10a043eeda16c16f414990
Eli Scott <scottej1@gmail.com>
2026-08-07 20:11:40 +0000

    Build and Lint Fixes

--- src/app/(admin)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import AdminLayout from "@/components/layout/AdminLayout";
-
-export default function AdminSegmentLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <AdminLayout>{children}</AdminLayout>;
-}
